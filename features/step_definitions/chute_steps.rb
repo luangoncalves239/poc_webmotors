611618a98c8d8a636eadea3a3950b1f023db355c
Joao Paulo <jopnascimento.essence@rd.com.br>
2021-08-06 19:08:58 -0300

    Fix

--- features/step_definitions/chute_steps.rb
+++ features/step_definitions/chute_steps.rb
@@ -1,18 +1,18 @@
 Dado(/^que eu consulte o contrato make de marcas$/) do
-    @retorno = online_challenge.consulta_make
+  @retorno = online_challenge.consulta_make
 end
 
-Entao(/^deve retorna com o valor "([^"]*)" disponivel$/) do |valor_esperado|
+Entao(/^deve retorna com o valor "(.*)" disponivel$/) do |valor_esperado|
   expect(@retorno.code).to eql(200)
   retorno = online_challenge.localizar_modelo_marca(@retorno, valor_esperado)
 end
 
-Quando(/^consultar o contrato model com a marca "([^"]*)"$/) do |marca_esperado|
+Quando(/^consultar o contrato model com a marca "(.*)"$/) do |marca_esperado|
   id_marca = online_challenge.localizar_modelo_marca(@retorno, marca_esperado)
   @retorno = online_challenge.consulta_por_id('model', id_marca)
 end
 
-Quando(/^consulta as versoes disponiveis do modelo "([^"]*)"$/) do |modelo|
+Quando(/^consulta as versoes disponiveis do modelo "(.*)"$/) do |modelo|
   id_model = online_challenge.localizar_modelo_marca(@retorno, modelo)
   @retorno = online_challenge.consulta_por_id('version', id_model)
 end
